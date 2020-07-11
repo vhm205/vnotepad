00ff5ea43477864669ec725a23e1ce12bdae1636
VHM <taolaolataom@gmail.com>
2020-07-11 15:57:48 +0700

    Updates

---
+++ precache-manifest.3297aa28bcbb8d80ec379051ea2fe2cd.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3fc36ce379040775da1713b6496b16ed",
+    "url": "/vnotepad/index.html"
+  },
+  {
+    "revision": "68333fdad0fe3ee8491d",
+    "url": "/vnotepad/static/js/2.c81cb088.chunk.js"
+  },
+  {
+    "revision": "bbc437e30d7cbbcd12a23711abb4013f",
+    "url": "/vnotepad/static/js/2.c81cb088.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "efbd133a6561b8c4be52",
+    "url": "/vnotepad/static/js/main.8169d503.chunk.js"
+  },
+  {
+    "revision": "56cd7b5dd706c1d5a57f",
+    "url": "/vnotepad/static/js/runtime-main.d6e88d94.js"
+  }
+]);
